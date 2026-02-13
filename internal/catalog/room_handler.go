b41e6417a691c10d0b86d6ae8f391823425eaf12
mukhtaruly <nuratop40@gmail.com>
2026-02-13 05:11:46 +0500

    nurassyl

--- internal/catalog/room_handler.go
+++ internal/catalog/room_handler.go
@@ -259,3 +259,4 @@ func (h *RoomHandler) CheckAvailability(w http.ResponseWriter, r *http.Request) 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(available)
 }
+//nur
